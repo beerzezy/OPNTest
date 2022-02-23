5ea8a1d065be6e2d334c278a0e69e53c54c38500
beerzezy <phanudetkawilai2016@gmail.com>
2022-02-23 13:26:01 +0700

    unit test FootballRankingCalculate

--- assignment3/footballrankingcalculate_test.go
+++ assignment3/footballrankingcalculate_test.go
@@ -1,1 +1,127 @@
 package assignment3
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_calculateStats(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		input    []inputTeamStats
+		expected []inputTeamStats
+	}{
+		{
+			input: []inputTeamStats{
+				{
+					TeamName:    "D",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					GoalFor:     15,
+					GoalAgainst: 11,
+				},
+				{
+					TeamName:    "B",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					GoalFor:     24,
+					GoalAgainst: 20,
+				},
+			},
+			expected: []inputTeamStats{
+				{
+					TeamName:    "D",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					Score:       19,
+					GoalFor:     15,
+					GoalAgainst: 11,
+					GoalDiff:    4,
+				},
+				{
+					TeamName:    "B",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					Score:       19,
+					GoalFor:     24,
+					GoalAgainst: 20,
+					GoalDiff:    4,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := calculateStats(tt.input)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func Test_calculateRanking(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		input    []inputTeamStats
+		expected []resultTeamStat
+	}{
+		{
+			input: []inputTeamStats{
+				{
+					TeamName:    "D",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					Score:       19,
+					GoalFor:     15,
+					GoalAgainst: 11,
+					GoalDiff:    4,
+				},
+				{
+					TeamName:    "B",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					Score:       19,
+					GoalFor:     24,
+					GoalAgainst: 20,
+					GoalDiff:    4,
+				},
+			},
+			expected: []resultTeamStat{
+				{
+					TeamName:    "B",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					Score:       19,
+					GoalFor:     24,
+					GoalAgainst: 20,
+					GoalDiff:    4,
+				},
+				{
+					TeamName:    "D",
+					Win:         6,
+					Lose:        1,
+					Draw:        1,
+					Score:       19,
+					GoalFor:     15,
+					GoalAgainst: 11,
+					GoalDiff:    4,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := calculateRanking(tt.input)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
